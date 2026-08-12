9ef5dcf55804faad5fe0d4cb775f965780b772e2
SUSAN AWORI <susanawori15@gmail.com>
2026-08-12 07:17:37 +0300

    Create a numpy and add user to enter the number of students and subjects

---
+++ student_grade.py
@@ -0,0 +1,15 @@
+import numpy as np
+def calculate_grades():
+    print("=" * 50)
+    print("       STUDENT GRADE CALCULATOR (NumPy)")
+    print("=" * 50)
+
+    try:
+        num_students = int(input("Enter the number of students: "))
+        num_subjects = int(input("Enter the number of subjects: "))
+        if num_students <= 0 or num_subjects <= 0:
+            print("Number of students and subjects must be greater than zero.")
+            return
+    except ValueError:
+        print("Invalid input. Please enter integer values.")
+        return
